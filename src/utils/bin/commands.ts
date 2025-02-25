176583a655c06a5f1ca270155a011b18696dc46b
shouryashashank <shouryashashank@gmail.com>
2025-02-25 22:21:59 +0530

    added rf

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -191,6 +191,11 @@ export const sudo = async (args?: string[]): Promise<string> => {
   return `Permission denied: with little power comes... no responsibility? `;
 };
 
+export const rm = async (args?: string[]): Promise<string> => {
+  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
+  return `Permission denied: with little power comes... no responsibility? `;
+};
+
 // Banner
 export const banner = (args?: string[]): string => {
   return `
